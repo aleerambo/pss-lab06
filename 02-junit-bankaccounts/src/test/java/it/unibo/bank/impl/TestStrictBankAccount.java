8142832054f41084f7bedf87358c47f154d1c509
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2024-11-12 11:52:17 +0100

    UPDATE TestStrictBankAccount tests

--- 02-junit-bankaccounts/src/test/java/it/unibo/bank/impl/TestStrictBankAccount.java
+++ 02-junit-bankaccounts/src/test/java/it/unibo/bank/impl/TestStrictBankAccount.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+//import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Assertions;
 
