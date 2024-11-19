4feeafb0324d1ddba574ba23454abb0e4b43647c
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2024-11-19 10:17:36 +0100

    UPDATE FizzBuzz class with new requirements

--- 03-tdd/src/test/java/it/unibo/tdd/FizzBuzz.java
+++ 03-tdd/src/test/java/it/unibo/tdd/FizzBuzz.java
@@ -2,17 +2,26 @@ package it.unibo.tdd;
 
 public class FizzBuzz {
     public String getValue(int i) {
-        if (i % 3 == 0 && i % 5 == 0) {
-                return "FizzBuzz";
-            } 
-            else if (i % 3 == 0) {
-                return "Fizz";
-            } 
-            else if (i % 5 == 0) {
-                return "Buzz";
-            } 
-            else {
-                return String.valueOf(i);
-            }
+        if (String.valueOf(i).contains("3") && String.valueOf(i).contains("5")) {
+            return "FizzBuzz";
+        }
+        else if(String.valueOf(i).contains("3")) {
+            return "Fizz";
+        }
+        else if(String.valueOf(i).contains("5")) {
+            return "Buzz";
+        }
+        else if (i % 3 == 0 && i % 5 == 0) {
+            return "FizzBuzz";
+        } 
+        else if (i % 3 == 0) {
+            return "Fizz";
+        } 
+        else if (i % 5 == 0) {
+            return "Buzz";
+        } 
+        else {
+            return String.valueOf(i);
+        }
     }
 }
