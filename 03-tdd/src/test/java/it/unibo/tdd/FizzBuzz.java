efd8eb25ced1de39653e9c7416770dc54e6689ab
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2024-11-19 10:28:35 +0100

    REFACTORING FizzBuzz class

--- 03-tdd/src/test/java/it/unibo/tdd/FizzBuzz.java
+++ 03-tdd/src/test/java/it/unibo/tdd/FizzBuzz.java
@@ -2,22 +2,13 @@ package it.unibo.tdd;
 
 public class FizzBuzz {
     public String getValue(int i) {
-        if (String.valueOf(i).contains("3") && String.valueOf(i).contains("5")) {
-            return "FizzBuzz";
-        }
-        else if(String.valueOf(i).contains("3")) {
-            return "Fizz";
-        }
-        else if(String.valueOf(i).contains("5")) {
-            return "Buzz";
-        }
-        else if (i % 3 == 0 && i % 5 == 0) {
+        if ( (i % 3 == 0 && i % 5 == 0) || (String.valueOf(i).contains("3") && String.valueOf(i).contains("5")) ) {
             return "FizzBuzz";
         } 
-        else if (i % 3 == 0) {
+        else if ( (i % 3 == 0) || (String.valueOf(i).contains("3")) ) {
             return "Fizz";
         } 
-        else if (i % 5 == 0) {
+        else if ( (i % 5 == 0) || (String.valueOf(i).contains("5")) ) {
             return "Buzz";
         } 
         else {
